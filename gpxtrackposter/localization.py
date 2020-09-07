46484b5c8745021480d368e4ce117c9d4fa6a600
Florian Pigorsch <fpigorsch@gmail.com>
2020-09-07 13:03:55 +0200

    Add proper month localization via explicitly translated month names. Add --localedir command line option.

--- gpxtrackposter/localization.py
+++ gpxtrackposter/localization.py
@@ -6,35 +6,6 @@
 import locale
 
 
-# month: 1-12 (1=January)
-def localized_month_name(month: int) -> str:
-    assert 1 <= month <= 12
-
-    # locale_name = locale.getlocale()[0]
-
-    # special case for russian
-    # if locale_name == "ru_RU":
-    #    return ...
-
-    # default
-    return locale.nl_langinfo(
-        [
-            locale.MON_1,
-            locale.MON_2,
-            locale.MON_3,
-            locale.MON_4,
-            locale.MON_5,
-            locale.MON_6,
-            locale.MON_7,
-            locale.MON_8,
-            locale.MON_9,
-            locale.MON_10,
-            locale.MON_11,
-            locale.MON_12,
-        ][month - 1]
-    )
-
-
 # day_of_week: 0-6 (0=Monday)
 # If short is False return the full day name, otherwise return the shortest
 # possible abbreviation (e.g. the first letter)
