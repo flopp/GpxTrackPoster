555acaec0c9ea6cc0cb720f9fd5a001e8fdc049d
Lowtower <lowtower@gmx.de>
2021-12-02 15:06:46 +0100

    add second test to catch times with utc offset

--- tests/test_timezone_adjuster.py
+++ tests/test_timezone_adjuster.py
@@ -3,8 +3,8 @@
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
-import dateutil.parser
 import s2sphere  # type: ignore
+from dateutil import parser, tz
 
 from gpxtrackposter.timezone_adjuster import TimezoneAdjuster
 
@@ -12,7 +12,7 @@ from gpxtrackposter.timezone_adjuster import TimezoneAdjuster
 def test_adjust() -> None:
     tza = TimezoneAdjuster()
 
-    time = dateutil.parser.parse("2020-09-06T14:34:01.029Z")
+    time = parser.parse("2020-09-06T14:34:01.029Z")
 
     freiburg = s2sphere.LatLng.from_degrees(47.998933, 7.841819)
     time_freiburg = tza.adjust(time, freiburg)
@@ -21,3 +21,18 @@ def test_adjust() -> None:
     newyork = s2sphere.LatLng.from_degrees(40.711344, -74.005382)
     time_newyork = tza.adjust(time, newyork)
     assert time_newyork.hour == 10
+
+
+def test_adjust_with_utc_returns_time() -> None:
+    tza = TimezoneAdjuster()
+
+    tzinfos = {"CDT": tz.gettz("US/Central")}
+    time = parser.parse("2020-09-06T14:34:01.029 CDT", tzinfos=tzinfos)
+
+    freiburg = s2sphere.LatLng.from_degrees(47.998933, 7.841819)
+    time_freiburg = tza.adjust(time, freiburg)
+    assert time_freiburg.hour == 14
+
+    newyork = s2sphere.LatLng.from_degrees(40.711344, -74.005382)
+    time_newyork = tza.adjust(time, newyork)
+    assert time_newyork.hour == 14
