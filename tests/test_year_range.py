12af1fc2c89d936e3113d2a09a134de8dc9a9764
Lowtower <lowtower@gmx.de>
2021-12-08 16:44:05 +0100

    make use of SubTest expected result comes first in unittest.assert...

--- tests/test_year_range.py
+++ tests/test_year_range.py
@@ -27,29 +27,32 @@ class TestCase(unittest.TestCase):
         """parse with valid strings returns True"""
         valid_strings = [("all", None, None), ("2016", 2016, 2016), ("2018", 2018, 2018), ("2016-2018", 2016, 2018)]
         for string, from_year, to_year in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            self.assertEqual(year_range.from_year, from_year)
-            self.assertEqual(year_range.to_year, to_year)
+            with self.subTest(f"{string} -> {from_year}-{to_year}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                self.assertEqual(from_year, year_range.from_year)
+                self.assertEqual(to_year, year_range.to_year)
 
     def test_parse_with_invalid_strings_returns_false(self) -> None:
         """parse with invalid strings returns False"""
         valid_strings = ["20xx", "20xx-2018", "2016:2018"]
         for string in valid_strings:
-            year_range = YearRange()
-            self.assertFalse(year_range.parse(string))
+            with self.subTest(f"{string}"):
+                year_range = YearRange()
+                self.assertFalse(year_range.parse(string))
 
     def test_clear_returns_empty_instance(self) -> None:
         """clear returns empty instance of YearRange"""
         valid_strings = ["2016", "2018", "2016-2018"]
         for string in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            self.assertIsNotNone(year_range.from_year)
-            self.assertIsNotNone(year_range.to_year)
-            year_range.clear()
-            self.assertIsNone(year_range.from_year)
-            self.assertIsNone(year_range.to_year)
+            with self.subTest(f"{string}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                self.assertIsNotNone(year_range.from_year)
+                self.assertIsNotNone(year_range.to_year)
+                year_range.clear()
+                self.assertIsNone(year_range.from_year)
+                self.assertIsNone(year_range.to_year)
 
     def test_add_increases_year_range(self) -> None:
         """add with higher/lower values increases YearRange"""
@@ -61,11 +64,12 @@ class TestCase(unittest.TestCase):
             ("2016-2018", datetime(2019, 1, 1), 2016, 2019),
         ]
         for string, added, from_year, to_year in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            year_range.add(added)
-            self.assertEqual(year_range.from_year, from_year)
-            self.assertEqual(year_range.to_year, to_year)
+            with self.subTest(f"{string}, {added} -> {from_year}-{to_year}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                year_range.add(added)
+                self.assertEqual(from_year, year_range.from_year)
+                self.assertEqual(to_year, year_range.to_year)
 
     def test_contains_returns_true(self) -> None:
         """contains returns True"""
@@ -77,9 +81,10 @@ class TestCase(unittest.TestCase):
             ("2016-2018", datetime(2018, 1, 1)),
         ]
         for string, value in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            self.assertTrue(year_range.contains(value))
+            with self.subTest(f"{string}, {value}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                self.assertTrue(year_range.contains(value))
 
     def test_contains_returns_false(self) -> None:
         """contains returns False"""
@@ -90,14 +95,15 @@ class TestCase(unittest.TestCase):
             ("2016-2018", datetime(2019, 1, 1)),
         ]
         for string, value in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            self.assertFalse(year_range.contains(value))
+            with self.subTest(f"{string}, {value}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                self.assertFalse(year_range.contains(value))
 
     def test_count_with_empty_year_range_returns_zero(self) -> None:
         """count with empty year range returns 0"""
         year_range = YearRange()
-        self.assertEqual(year_range.count(), 0)
+        self.assertEqual(0, year_range.count())
 
     def test_count_returns_value(self) -> None:
         """count returns value"""
@@ -107,9 +113,10 @@ class TestCase(unittest.TestCase):
             ("2015-2019", 5),
         ]
         for string, expected_value in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            self.assertTrue(year_range.count(), expected_value)
+            with self.subTest(f"{string} -> {expected_value}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                self.assertEqual(expected_value, year_range.count())
 
     def test_iter_with_empty_year_range_returns(self) -> None:
         """iter with empty year range returns"""
@@ -117,7 +124,7 @@ class TestCase(unittest.TestCase):
         list_of_years = []
         for year in year_range.iter():
             list_of_years.append(year)
-        self.assertListEqual(list_of_years, [])
+        self.assertListEqual([], list_of_years)
 
     def test_iter_returns_values(self) -> None:
         """iter returns YearRange values"""
@@ -127,12 +134,13 @@ class TestCase(unittest.TestCase):
             ("2015-2019", [2015, 2016, 2017, 2018, 2019]),
         ]
         for string, expected_values in valid_strings:
-            year_range = YearRange()
-            self.assertTrue(year_range.parse(string))
-            list_of_years = []
-            for year in year_range.iter():
-                list_of_years.append(year)
-            self.assertListEqual(list_of_years, expected_values)
+            with self.subTest(f"{string} -> {expected_values}"):
+                year_range = YearRange()
+                self.assertTrue(year_range.parse(string))
+                list_of_years = []
+                for year in year_range.iter():
+                    list_of_years.append(year)
+                self.assertListEqual(expected_values, list_of_years)
 
 
 if __name__ == "__main__":
