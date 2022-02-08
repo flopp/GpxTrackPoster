9728751a3654ae4fbc17c2503ea30c728e37985c
Lowtower <lowtower@lowtower.de>
2022-02-08 16:58:18 +0100

    remove pylint ignore

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,8 +6,6 @@ Several tests for entry point cli.py
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
 
-# pylint: disable=redefined-outer-name
-
 import argparse
 import datetime
 import logging
