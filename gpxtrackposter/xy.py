3485d5aa750470c769a09df1a27a5603839556c0
Lowtower <lowtower@lowtower.de>
2022-04-14 15:37:06 +0200

    - bump-year to 2022

--- gpxtrackposter/xy.py
+++ gpxtrackposter/xy.py
@@ -1,5 +1,5 @@
 """Represent x,y coords with properly overloaded operations."""
-# Copyright 2016-2021 Florian Pigorsch & Contributors. All rights reserved.
+# Copyright 2016-2022 Florian Pigorsch & Contributors. All rights reserved.
 #
 # Use of this source code is governed by a MIT-style
 # license that can be found in the LICENSE file.
