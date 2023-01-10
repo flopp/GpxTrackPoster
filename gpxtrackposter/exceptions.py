6f19da1e329f32cc1b577a618b0a444743f91268
Lowtower <lowtower@lowtower.de>
2023-01-10 17:27:28 +0100

    make use of setup.cfg instead of several specific config files

--- gpxtrackposter/exceptions.py
+++ gpxtrackposter/exceptions.py
@@ -1,3 +1,4 @@
+"""Exceptions"""
 # Copyright 2016-2023 Florian Pigorsch & Contributors. All rights reserved.
 #
 # Use of this source code is governed by a MIT-style
@@ -5,12 +6,12 @@
 
 
 class PosterError(Exception):
-    "Base class for all errors"
+    """Base class for all errors"""
 
 
 class TrackLoadError(PosterError):
-    "Something went wrong when loading a track file"
+    """Something went wrong when loading a track file"""
 
 
 class ParameterError(PosterError):
-    "Something's wrong with user supplied parameters"
+    """Something's wrong with user supplied parameters"""
